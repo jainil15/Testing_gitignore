c2155f156b9955f72919d655b9ceecbf96f7e7d3
jainil15 <jainilpatel115@gmail.com>
2024-02-15 22:55:30 +0530

    Added Readme, assets, modified main.tf

---
+++ README.md
@@ -0,0 +1,5 @@
+# Terraform
+
+- `Terraform init`
+- `Terraform plan`![alt text](image.png)
+- `Terraform apply`
--- main.tf
+++ main.tf
@@ -2,15 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.36.0"
+      version = "~> 5.0"
     }
   }
-
 }
 terraform {
   cloud {
     organization = "Jainil-Org"
-
     workspaces {
       name = "ForAssignment"
     }
@@ -56,13 +54,25 @@ resource "aws_security_group" "allow_http_and_ssh" {
     from_port        = 22
     to_port          = 22
   }
+  egress {
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    protocol         = "-1"
+    from_port        = 0
+    to_port          = 0
+  }
 }
 
 resource "aws_instance" "app_server" {
   ami                    = "ami-06b72b3b2a773be2b"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_http_and_ssh.id]
-  user_data              = <<EOF
+
+  key_name = aws_key_pair.mykeypair.key_name
+  tags = {
+    Name = "${local.name} ${local.owner}"
+  }
+  user_data = <<EOF
 #!bin/bash
 yum update -y
 yum install -y httpd
@@ -71,9 +81,6 @@ systemctl enable httpd
 echo "<h1>HELLO WORLD FROM $(hostname -f)</h1>" > /var/www/html/index.html
 systemctl restart httpd
   EOF
-  tags = {
-    Name = "${local.name} ${local.owner}"
-  }
 }
 
 output "instance_ip_address" {
